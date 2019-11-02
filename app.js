85108e5f4b6decdb8eea383fa6055c647dba7049
shAkeN_1 <1031975978@qq.com>
2019-11-02 21:41:48 +0800

    make the plan:planIndex

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ App({
           let capsule = wx.getMenuButtonBoundingClientRect();
           if (capsule) {
             this.globalData.Custom = capsule;
-            this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+            //this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
           } else {
-            this.globalData.CustomBar = e.statusBarHeight + 50;
+            //this.globalData.CustomBar = e.statusBarHeight + 50;
           }
         }
       })
